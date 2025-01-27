e91fa2740bffb2aa96ba394cb87ae2942786e1fc
Tristesse02 <mtvu@umass.edu>
2025-01-27 00:35:31 -0500

    Adding template for jinja2 and successfully parse resume with JSON content

---
+++ autofill.py
@@ -0,0 +1,35 @@
+import json
+from jinja2 import Template
+
+def escape_latex(value):
+    """Escape LaTeX special characters."""
+    if isinstance(value, str):
+        return value.replace('%', r'\%').replace('_', r'\_').replace('&', r'\&').replace('#', r'\#')
+    return value
+
+def recursive_escape(obj):
+    if isinstance(obj, dict):
+        return {k: recursive_escape(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [recursive_escape(v) for v in obj]
+    elif isinstance(obj, str):
+        return escape_latex(obj)
+    return obj
+
+# Load the JSON file
+with open("personalData.json") as f:
+    data = json.load(f)
+    
+data = recursive_escape(data)
+
+# Load the LaTeX template
+with open("templateResume.tex.j2") as f:
+    template = Template(f.read())
+
+rendered_latex = template.render(data)
+
+# Write the output to a new LaTeX file
+with open("output.tex", "w") as f:
+    f.write(rendered_latex)
+
+print("LaTeX file generated successfully at: output.tex")
