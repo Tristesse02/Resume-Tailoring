727a966fe75accda46b769291f5f51c8cdd95558
Tristesse02 <mtvu@umass.edu>
2025-04-25 10:41:29 -0400

    Change UI for University page

--- my-resume-app/src/components/pages/UniversityInfo.jsx
+++ my-resume-app/src/components/pages/UniversityInfo.jsx
@@ -1,6 +1,7 @@
 import styles from "./index.module.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { ArrowLeft, ArrowRight, GraduationCap } from "lucide-react";
 
 const UniversityInfo = () => {
   const navigate = useNavigate();
@@ -39,68 +40,105 @@ const UniversityInfo = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Profile Information</h1>
+    <div className={styles.profileContainer}>
+      <div className={styles.profileHeaderContainer}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+            justifyContent: "flex-start",
+          }}
+        >
+          <GraduationCap style={{ height: "20px", width: "20px" }} />
+          <h3 className={styles.profileHeaderHeader}>Education Information</h3>
+        </div>
+        <p className={styles.profileHeaderTitle}>
+          Enter your educational background to include in your resume
+        </p>
+      </div>
       <form className={styles.form}>
-        <input
-          className={styles.input}
-          type="text"
-          name="university"
-          placeholder="University"
-          value={formData.university}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={styles.input}
-          type="text"
-          name="degree"
-          placeholder="Degree"
-          value={formData.degree}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={styles.input}
-          type="text"
-          name="gpa"
-          placeholder="GPA"
-          value={formData.gpa}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={styles.input}
-          type="text"
-          name="graduate"
-          placeholder="Graduation Year"
-          value={formData.graduate}
-          onChange={handleChange}
-          required
-        />
-        <input
-          className={styles.input}
-          type="text"
-          name="location"
-          placeholder="University Location"
-          value={formData.location}
-          onChange={handleChange}
-        />
-        <textarea
-          className={styles.textarea}
-          type="text"
-          name="courses"
-          placeholder="Courses"
-          value={formData.courses}
-          onChange={handleChange}
-        />
+        <div className={styles.educationFormGrid}>
+          <div className={styles.fullWidthSpan3}>
+            <label>University</label>
+            <input
+              className={styles.profileInput}
+              type="text"
+              name="university"
+              placeholder="Stanford University"
+              value={formData.university}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.fullWidthSpan3}>
+            <label>Degree</label>
+            <input
+              className={styles.profileInput}
+              type="text"
+              name="degree"
+              placeholder="B.S. Computer Science"
+              value={formData.degree}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.fullWidthSpan2}>
+            <label>GPA</label>
+            <input
+              className={styles.profileInput}
+              type="text"
+              name="gpa"
+              placeholder="3.8"
+              value={formData.gpa}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.fullWidthSpan2}>
+            <label>Graduation Year</label>
+            <input
+              className={styles.profileInput}
+              type="text"
+              name="graduate"
+              placeholder="2025"
+              value={formData.graduate}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.fullWidthSpan2}>
+            <label>Location</label>
+            <input
+              className={styles.profileInput}
+              type="text"
+              name="Location"
+              placeholder="Stanford, CA"
+              value={formData.location}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.fullWidthSpan6}>
+            <label>Relevant Courses</label>
+            <textarea
+              className={styles.profileTextarea}
+              type="text"
+              name="courses"
+              placeholder="Data Structures, Algorithms, Machine Learning, etc."
+              value={formData.courses}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
       </form>
       <div className={styles.buttonContainer}>
         <button onClick={handlePreviousPage} className={styles.buttonLeft}>
-          Back ⬅
+          <ArrowLeft className={styles.buttonArrowLeft} />
+          Back
         </button>
         <button onClick={handleNextPage} className={styles.buttonRight}>
-          Next ➡
+          Next
+          <ArrowRight className={styles.buttonArrowRight} />
         </button>
       </div>
     </div>
